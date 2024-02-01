26b313215887d4585ca416b9bb726043e2f069e1
Choiyu330 <jenny4063@naver.com>
2024-02-01 23:51:02 +0900

    How to receive Parameters

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def say_hello():
     print("Hello How r u?")
 
+# How to receive Parameters
+def say_hello(name):
+    print("Hello", name, ", How r u?")
 
-say_hello()
+say_hello("yujin")
+say_hello("anonymous")
