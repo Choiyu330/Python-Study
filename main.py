ef28b7f3817d30eba733a3bc5108d68f9e099260
Choiyu330 <jenny4063@naver.com>
2024-02-04 23:58:35 +0900

    Default Parameters

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ def say_hello(name, age):
     print("You are", age, "years old")
     
 say_hello("Anonymous", 26)
+
+# Default Parameters
+def say_hello2(name="Anonymous"):
+    print("HELLO !", name)
+
+say_hello2()
